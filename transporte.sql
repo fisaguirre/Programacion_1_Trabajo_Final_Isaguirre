6777e5c81804503d39a20d9a53aaa856d7365ed5
Fernando-Isaguirre <feragustin-13@hotmail.com>
2018-12-05 16:24:43 -0300

    DB

---
+++ transporte.sql
@@ -0,0 +1,105 @@
+/*
+SQLyog Ultimate v12.09 (32 bit)
+MySQL - 5.7.24 : Database - transporte
+*********************************************************************
+*/
+
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`transporte` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `transporte`;
+
+/*Table structure for table `auditoria` */
+
+DROP TABLE IF EXISTS `auditoria`;
+
+CREATE TABLE `auditoria` (
+  `auditoria_id` int(4) NOT NULL AUTO_INCREMENT,
+  `fecha_acceso` date DEFAULT NULL,
+  `user` varchar(30) NOT NULL DEFAULT '',
+  `response_time` int(4) NOT NULL DEFAULT '0' COMMENT 'tiempo en milisegundos',
+  `endpoint` varchar(150) NOT NULL DEFAULT '',
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`auditoria_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `chofer` */
+
+DROP TABLE IF EXISTS `chofer`;
+
+CREATE TABLE `chofer` (
+  `chofer_id` int(4) NOT NULL AUTO_INCREMENT,
+  `apellido` varchar(100) NOT NULL DEFAULT '',
+  `nombre` varchar(100) NOT NULL DEFAULT '',
+  `documento` decimal(11,0) NOT NULL DEFAULT '0',
+  `email` varchar(100) NOT NULL DEFAULT '',
+  `vehiculo_id` int(4) DEFAULT NULL,
+  `sistema_id` int(4) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`chofer_id`),
+  KEY `documento` (`documento`),
+  KEY `vehiculo_id` (`vehiculo_id`),
+  KEY `sistema_id` (`sistema_id`),
+  CONSTRAINT `chofer_ibfk_1` FOREIGN KEY (`vehiculo_id`) REFERENCES `vehiculo` (`vehiculo_id`),
+  CONSTRAINT `chofer_ibfk_2` FOREIGN KEY (`sistema_id`) REFERENCES `sistema_transporte` (`sistema_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `sistema_transporte` */
+
+DROP TABLE IF EXISTS `sistema_transporte`;
+
+CREATE TABLE `sistema_transporte` (
+  `sistema_id` int(4) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL DEFAULT '',
+  `pais_procedencia` varchar(100) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`sistema_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `sistema_vehiculo` */
+
+DROP TABLE IF EXISTS `sistema_vehiculo`;
+
+CREATE TABLE `sistema_vehiculo` (
+  `sistemavehiculo_id` int(4) NOT NULL AUTO_INCREMENT,
+  `vehiculo_id` int(4) DEFAULT NULL,
+  `sistema_id` int(4) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`sistemavehiculo_id`),
+  UNIQUE KEY `vehiculo_id` (`vehiculo_id`,`sistema_id`),
+  KEY `sistema_id` (`sistema_id`),
+  CONSTRAINT `sistema_vehiculo_ibfk_1` FOREIGN KEY (`vehiculo_id`) REFERENCES `vehiculo` (`vehiculo_id`),
+  CONSTRAINT `sistema_vehiculo_ibfk_2` FOREIGN KEY (`sistema_id`) REFERENCES `sistema_transporte` (`sistema_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `vehiculo` */
+
+DROP TABLE IF EXISTS `vehiculo`;
+
+CREATE TABLE `vehiculo` (
+  `vehiculo_id` int(4) NOT NULL AUTO_INCREMENT,
+  `patente` varchar(10) NOT NULL DEFAULT '',
+  `anho_patente` smallint(2) NOT NULL DEFAULT '0',
+  `anho_fabricacion` smallint(2) NOT NULL DEFAULT '0',
+  `marca` varchar(100) DEFAULT NULL,
+  `modelo` varchar(100) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`vehiculo_id`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;

987e11cb567b7f07792d2041b80b2dc8a182a4fe
Fernando-Isaguirre <feragustin-13@hotmail.com>
2018-12-05 19:37:01 -0300

    db

--- transporte.sql
+++ transporte.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 05-12-2018 a las 17:30:59
+-- Tiempo de generación: 05-12-2018 a las 20:35:57
 -- Versión del servidor: 5.7.24-0ubuntu0.16.04.1
 -- Versión de PHP: 7.0.32-0ubuntu0.16.04.1
 
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `transporte`
 --
+CREATE DATABASE IF NOT EXISTS `transporte` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `transporte`;
 
 -- --------------------------------------------------------
 
@@ -26,24 +28,23 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `auditoria`
 --
 
-CREATE TABLE `auditoria` (
-  `auditoria_id` int(4) NOT NULL,
+DROP TABLE IF EXISTS `auditoria`;
+CREATE TABLE IF NOT EXISTS `auditoria` (
+  `auditoria_id` int(4) NOT NULL AUTO_INCREMENT,
   `fecha_acceso` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `user` varchar(30) NOT NULL DEFAULT '',
   `response_time` float NOT NULL DEFAULT '0' COMMENT 'tiempo en milisegundos',
   `endpoint` varchar(300) NOT NULL DEFAULT '',
-  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`auditoria_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `auditoria`
 --
 
 INSERT INTO `auditoria` (`auditoria_id`, `fecha_acceso`, `user`, `response_time`, `endpoint`, `created`) VALUES
-(1, '2018-12-05 19:16:02', 'fer', 0.0368, 'localhost/2018/Trabajo_Final/sistema_transporte/crud.php/POST?jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIiLCJhdWQiOiIiLCJpYXQiOjEzNTY5OTk1MjQsIm5iZiI6MTM1NzAwMDAwMCwiZGF0YSI6eyJ1c3VhcmlvX2lkIjoiOSIsInVzdWFyaW8iOiJqZXJvIn19.DpnKPcNaAH1fdvzvQUzpUkkSzM5FF2kbcPHXaYxVR4w', '2018-12-05 19:16:02'),
-(2, '2018-12-05 19:16:13', 'fer', 0.032, 'localhost/2018/Trabajo_Final/sistema_transporte/crud.php/POST?jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIiLCJhdWQiOiIiLCJpYXQiOjEzNTY5OTk1MjQsIm5iZiI6MTM1NzAwMDAwMCwiZGF0YSI6eyJ1c3VhcmlvX2lkIjoiOSIsInVzdWFyaW8iOiJqZXJvIn19.DpnKPcNaAH1fdvzvQUzpUkkSzM5FF2kbcPHXaYxVR4w', '2018-12-05 19:16:13'),
-(3, '2018-12-05 19:16:50', 'fer', 0.0656, 'localhost/2018/Trabajo_Final/sistema_transporte/crud.php/POST?jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIiLCJhdWQiOiIiLCJpYXQiOjEzNTY5OTk1MjQsIm5iZiI6MTM1NzAwMDAwMCwiZGF0YSI6eyJ1c3VhcmlvX2lkIjoiOSIsInVzdWFyaW8iOiJqZXJvIn19.DpnKPcNaAH1fdvzvQUzpUkkSzM5FF2kbcPHXaYxVR4w', '2018-12-05 19:16:50'),
-(4, '2018-12-05 19:17:26', 'fer', 0.0572, 'localhost/2018/Trabajo_Final/sistema_transporte/crud.php/POST?jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIiLCJhdWQiOiIiLCJpYXQiOjEzNTY5OTk1MjQsIm5iZiI6MTM1NzAwMDAwMCwiZGF0YSI6eyJ1c3VhcmlvX2lkIjoiOSIsInVzdWFyaW8iOiJqZXJvIn19.DpnKPcNaAH1fdvzvQUzpUkkSzM5FF2kbcPHXaYxVR4w', '2018-12-05 19:17:26');
+(1, '2018-12-05 22:34:31', 'fer', 0.0011, 'localhost/2018/Trabajo_Final/chofer/crud.php//GET', '2018-12-05 22:34:31');
 
 -- --------------------------------------------------------
 
@@ -51,8 +52,9 @@ INSERT INTO `auditoria` (`auditoria_id`, `fecha_acceso`, `user`, `response_time`
 -- Estructura de tabla para la tabla `chofer`
 --
 
-CREATE TABLE `chofer` (
-  `chofer_id` int(4) NOT NULL,
+DROP TABLE IF EXISTS `chofer`;
+CREATE TABLE IF NOT EXISTS `chofer` (
+  `chofer_id` int(4) NOT NULL AUTO_INCREMENT,
   `apellido` varchar(100) NOT NULL DEFAULT '',
   `nombre` varchar(100) NOT NULL DEFAULT '',
   `documento` decimal(11,0) NOT NULL DEFAULT '0',
@@ -60,8 +62,14 @@ CREATE TABLE `chofer` (
   `vehiculo_id` int(4) DEFAULT NULL,
   `sistema_id` int(4) DEFAULT NULL,
   `created` datetime DEFAULT NULL,
-  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`chofer_id`),
+  KEY `documento` (`documento`),
+  KEY `vehiculo_id` (`vehiculo_id`),
+  KEY `sistema_id` (`sistema_id`),
+  KEY `vehiculo_id_2` (`vehiculo_id`),
+  KEY `vehiculo_id_3` (`vehiculo_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `chofer`
@@ -79,23 +87,27 @@ INSERT INTO `chofer` (`chofer_id`, `apellido`, `nombre`, `documento`, `email`, `
 -- Estructura de tabla para la tabla `sistema_transporte`
 --
 
-CREATE TABLE `sistema_transporte` (
-  `sistema_id` int(4) NOT NULL,
+DROP TABLE IF EXISTS `sistema_transporte`;
+CREATE TABLE IF NOT EXISTS `sistema_transporte` (
+  `sistema_id` int(4) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) NOT NULL DEFAULT '',
   `pais_procedencia` varchar(100) DEFAULT NULL,
   `created` datetime DEFAULT NULL,
-  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`sistema_id`),
+  UNIQUE KEY `nombre_UNIQUE` (`nombre`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `sistema_transporte`
 --
 
 INSERT INTO `sistema_transporte` (`sistema_id`, `nombre`, `pais_procedencia`, `created`, `updated`) VALUES
-(1, 'Taxi', 'Argentina', '2018-12-05 16:16:02', '2018-12-05 19:16:02'),
-(2, 'Uber', 'EEUU', '2018-12-05 16:16:13', '2018-12-05 19:16:13'),
-(3, 'Cabify.', 'España', '2018-12-05 16:16:50', '2018-12-05 19:16:50'),
-(4, 'Remis.', 'España', '2018-12-05 16:17:26', '2018-12-05 19:17:26');
+(1, 'uber', 'eeuu', '2018-12-04 20:39:16', '2018-12-05 02:39:16'),
+(2, 'taxi', 'argentina', '2018-12-04 20:39:34', '2018-12-05 02:39:34'),
+(3, 'cabifi', 'españa', '2018-12-04 20:39:42', '2018-12-05 02:39:42'),
+(4, 'uber pool', 'eeuu', '2018-12-04 20:39:52', '2018-12-05 02:39:52'),
+(5, 'remis', 'argentina', '2018-12-04 20:40:10', '2018-12-05 02:40:10');
 
 -- --------------------------------------------------------
 
@@ -103,13 +115,17 @@ INSERT INTO `sistema_transporte` (`sistema_id`, `nombre`, `pais_procedencia`, `c
 -- Estructura de tabla para la tabla `sistema_vehiculo`
 --
 
-CREATE TABLE `sistema_vehiculo` (
-  `sistemavehiculo_id` int(4) NOT NULL,
+DROP TABLE IF EXISTS `sistema_vehiculo`;
+CREATE TABLE IF NOT EXISTS `sistema_vehiculo` (
+  `sistemavehiculo_id` int(4) NOT NULL AUTO_INCREMENT,
   `vehiculo_id` int(4) DEFAULT NULL,
   `sistema_id` int(4) DEFAULT NULL,
   `created` datetime DEFAULT NULL,
-  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`sistemavehiculo_id`),
+  UNIQUE KEY `vehiculo_id` (`vehiculo_id`,`sistema_id`),
+  KEY `sistema_id` (`sistema_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `sistema_vehiculo`
@@ -131,14 +147,17 @@ INSERT INTO `sistema_vehiculo` (`sistemavehiculo_id`, `vehiculo_id`, `sistema_id
 -- Estructura de tabla para la tabla `usuarios`
 --
 
-CREATE TABLE `usuarios` (
-  `usuario_id` int(4) NOT NULL,
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `usuario_id` int(4) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(100) COLLATE utf8_spanish_ci NOT NULL DEFAULT '',
   `clave` varchar(100) COLLATE utf8_spanish_ci NOT NULL DEFAULT '',
   `rol` varchar(15) COLLATE utf8_spanish_ci NOT NULL,
   `created` datetime DEFAULT NULL,
-  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`usuario_id`),
+  UNIQUE KEY `nombre_UNIQUE` (`nombre`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `usuarios`
@@ -154,16 +173,19 @@ INSERT INTO `usuarios` (`usuario_id`, `nombre`, `clave`, `rol`, `created`, `upda
 -- Estructura de tabla para la tabla `vehiculo`
 --
 
-CREATE TABLE `vehiculo` (
-  `vehiculo_id` int(4) NOT NULL,
+DROP TABLE IF EXISTS `vehiculo`;
+CREATE TABLE IF NOT EXISTS `vehiculo` (
+  `vehiculo_id` int(4) NOT NULL AUTO_INCREMENT,
   `patente` varchar(10) NOT NULL DEFAULT '',
   `anho_patente` smallint(2) NOT NULL DEFAULT '0',
   `anho_fabricacion` smallint(2) NOT NULL DEFAULT '0',
   `marca` varchar(100) DEFAULT NULL,
   `modelo` varchar(100) DEFAULT NULL,
   `created` datetime DEFAULT NULL,
-  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`vehiculo_id`),
+  UNIQUE KEY `patente_UNIQUE` (`patente`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `vehiculo`
@@ -174,89 +196,6 @@ INSERT INTO `vehiculo` (`vehiculo_id`, `patente`, `anho_patente`, `anho_fabricac
 (2, 'ARG264', 2004, 2000, 'BMW', 'x5', '2018-12-04 20:41:28', '2018-12-05 02:41:28'),
 (3, 'ARS264', 2010, 1999, 'BMW', 'x5', '2018-12-04 20:42:00', '2018-12-05 02:52:52');
 
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `auditoria`
---
-ALTER TABLE `auditoria`
-  ADD PRIMARY KEY (`auditoria_id`);
-
---
--- Indices de la tabla `chofer`
---
-ALTER TABLE `chofer`
-  ADD PRIMARY KEY (`chofer_id`),
-  ADD KEY `documento` (`documento`),
-  ADD KEY `vehiculo_id` (`vehiculo_id`),
-  ADD KEY `sistema_id` (`sistema_id`),
-  ADD KEY `vehiculo_id_2` (`vehiculo_id`),
-  ADD KEY `vehiculo_id_3` (`vehiculo_id`);
-
---
--- Indices de la tabla `sistema_transporte`
---
-ALTER TABLE `sistema_transporte`
-  ADD PRIMARY KEY (`sistema_id`);
-
---
--- Indices de la tabla `sistema_vehiculo`
---
-ALTER TABLE `sistema_vehiculo`
-  ADD PRIMARY KEY (`sistemavehiculo_id`),
-  ADD UNIQUE KEY `vehiculo_id` (`vehiculo_id`,`sistema_id`),
-  ADD KEY `sistema_id` (`sistema_id`);
-
---
--- Indices de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`usuario_id`),
-  ADD UNIQUE KEY `nombre_UNIQUE` (`nombre`);
-
---
--- Indices de la tabla `vehiculo`
---
-ALTER TABLE `vehiculo`
-  ADD PRIMARY KEY (`vehiculo_id`),
-  ADD UNIQUE KEY `patente_UNIQUE` (`patente`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `auditoria`
---
-ALTER TABLE `auditoria`
-  MODIFY `auditoria_id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
---
--- AUTO_INCREMENT de la tabla `chofer`
---
-ALTER TABLE `chofer`
-  MODIFY `chofer_id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
---
--- AUTO_INCREMENT de la tabla `sistema_transporte`
---
-ALTER TABLE `sistema_transporte`
-  MODIFY `sistema_id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
---
--- AUTO_INCREMENT de la tabla `sistema_vehiculo`
---
-ALTER TABLE `sistema_vehiculo`
-  MODIFY `sistemavehiculo_id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
---
--- AUTO_INCREMENT de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `usuario_id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT de la tabla `vehiculo`
---
-ALTER TABLE `vehiculo`
-  MODIFY `vehiculo_id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- Restricciones para tablas volcadas
 --
